05dc6226ebc181b86976fc75a5309ca1f85e8700
Victor Ponce <vponce@uach.mx>
2022-06-30 12:39:47 -0600

    remove gitignore files y cache

--- node_modules/@kanety/jquery-simple-tree-table/src/consts.js
+++
@@ -1,1 +0,0 @@
-export const NAMESPACE = 'simple-tree-table';
--- node_modules/@kanety/jquery-simple-tree-table/test/config-spec.js
+++
@@ -1,9 +0,0 @@
-describe('jquery-simple-tree-table', () => {
-  it('config', () => {
-    let defaults = $.SimpleTreeTable.getDefaults();
-    expect(defaults.expander).toEqual(null);
-
-    defaults = $.SimpleTreeTable.setDefaults({expander: 'test'});
-    expect(defaults.expander).toEqual('test');
-  });
-});
