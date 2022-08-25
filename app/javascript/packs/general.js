369299ff4bd0edbbf92dd25889269d88bbb8de7f
Alejandro Rodriguez Gonzalez <alexrdgz03@gmail.com>
2022-08-25 13:04:06 -0600

    Se regresan los cambios a companies

--- app/javascript/packs/general.js
+++ app/javascript/packs/general.js
@@ -260,16 +260,6 @@ $(document).on('turbolinks:load', function () {
         }
     };
 
-    $(document).on("click", ".open-modal-file-request", function () {
-        var requestId       = $(this).data('id');
-
-        console.log("requestId -----------");
-        console.log(requestId);
-
-        $(".modal-body #requestIdFile").val(requestId);
-
-    });
-
     // $('#idFileEnlace').on("change", fileEnlace);
 
 
