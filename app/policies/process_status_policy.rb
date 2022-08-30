334f72c90c2c5a7c00506ec171ff5c60f4aa5576
DSI-Admin <javiert7ro@gmail.com>
2022-08-30 11:27:39 -0600

    Se agrega validaciones

--- app/policies/process_status_policy.rb
+++ app/policies/process_status_policy.rb
@@ -5,6 +5,8 @@ class ProcessStatusPolicy < ApplicationPolicy
         scope.all
       elsif @user.role_key.eql? 'analyst'
         scope.where(key: ['denied_analist', 'credit_validated'])
+      elsif @user.role_key.eql? 'credit_management'
+        scope.where(key: ['success_validated_period', 'rejected_by_credit_area', 'success_by_credit_area'])
       else
         nil
       end
