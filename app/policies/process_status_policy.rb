2c4ecfda4a4ff9e55a3ab43e0f3f88c951f4d6cb
Alejandro Rodriguez Gonzalez <alexrdgz03@gmail.com>
2022-09-13 12:28:49 -0600

    Se modifican los process_status en analust

--- app/policies/process_status_policy.rb
+++ app/policies/process_status_policy.rb
@@ -4,7 +4,7 @@ class ProcessStatusPolicy < ApplicationPolicy
       if @user.god?
         scope.all
       elsif @user.role_key.eql? 'analyst'
-        scope.where(key: ['denied_analist', 'credit_validated'])
+        scope.where(key: ['denied_analist', 'credit_validated','denied_validated_period','success_validated_period'])
       elsif @user.role_key.eql? 'credit_management'
         scope.where(key: ['success_validated_period', 'rejected_by_credit_area', 'success_by_credit_area'])
       elsif @user.role_key.eql? 'credit_area'
