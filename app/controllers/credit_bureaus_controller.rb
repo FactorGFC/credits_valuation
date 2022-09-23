7d44347b219c6a65e5e0b01bc7dabd796ae471ed
Jesus Lozano <jesusemanuel.254@gmail.com>
2022-09-23 14:12:31 -0600

    fix: Wrong namespace controller definition on app/controllers/api/v1/credit_bureaus_controller.rb

--- app/controllers/credit_bureaus_controller.rb
+++ app/controllers/credit_bureaus_controller.rb
@@ -1,4 +1,4 @@
-class CreditBureausController < ApplicationController
+class Api::V1::CreditBureausController < ApplicationController
   before_action :set_credit_bureau, only: %i[ edit update destroy ]
 
   # GET /credit_bureaus or /credit_bureaus.json
